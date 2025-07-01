feaac3c432404ab6010c6e1cc1e5969df37f0d2e
Katleho Myeza <katleho.myeza@bbd.co.za>
2025-07-01 20:47:54 +0200

    created post account endpoint

--- server/src/queries/accounts.queries.ts
+++ server/src/queries/accounts.queries.ts
@@ -5,3 +5,24 @@ import db from "../config/db.config";
 export const getAllAccounts = async (): Promise<string[]> => {
   return appConfig.isDev? db.any('SELECT account_number FROM accounts'): [];
 };
+
+export const createAccount = async (notification_url: string, created_at: Date, bank_id: Number): Promise<createAccountResponse> => {
+  const account = await db.one(
+    'INSERT INTO accounts (account_number,notification_url, created_at) VALUES (generate_unique_account_number(),$1, $2) RETURNING account_number,created_at',
+    [notification_url, created_at, bank_id]
+  );
+
+  return account;
+};
+
+export const insertAccountRef = async (account_number: string, bank_id: number): Promise<void> => {
+  await db.result(  
+    'INSERT INTO account_refs(account_number, bank_id) VALUES ($1, $2)',
+    [account_number, bank_id]
+  );
+}
+
+export type createAccountResponse = {
+  account_number: string;
+  created_at: Date;
+};
