ca4e2946f9eb3d195617a70d9c04656afca0375c
digir <dino.gironi@bbd.co.za>
2025-07-02 19:05:16 +0200

    Update migrations

--- flyway/live/V004__generate_number_functions.sql
+++ flyway/live/V004__generate_number_functions.sql
@@ -11,8 +11,8 @@ RETURNS VARCHAR(12) AS $$
 DECLARE new_acc_no VARCHAR(12);
 BEGIN
     LOOP
-        new_acc_no := '2000' || gen_4_digits() || gen_4_digits();
-        EXIT WHEN NOT EXISTS (SELECT 1 FROM accounts WHERE account_number = new_acc_no);
+      new_acc_no := '2000' || gen_4_digits() || gen_4_digits();
+      EXIT WHEN NOT EXISTS (SELECT 1 FROM accounts WHERE account_number = new_acc_no);
     END LOOP;
 
     RETURN new_acc_no;
@@ -40,10 +40,10 @@ CREATE OR REPLACE FUNCTION generate_unique_loan_number()
 RETURNS VARCHAR(16) AS $$
 DECLARE new_loan_no VARCHAR(16);
 BEGIN
-    LOOP
-        new_loan_no := gen_4_digits() || gen_4_digits() || gen_4_digits() || gen_4_digits();
-        EXIT WHEN NOT EXISTS (SELECT 1 FROM loans WHERE loan_number = new_loan_no);
-    END LOOP;
+  LOOP
+    new_loan_no := gen_4_digits() || gen_4_digits() || gen_4_digits() || gen_4_digits();
+    EXIT WHEN NOT EXISTS (SELECT 1 FROM loans WHERE loan_number = new_loan_no);
+  END LOOP;
 
     RETURN new_loan_no;
 END;
