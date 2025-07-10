608a6dba3e052074fab6c2f314817dd7d6737c18
Katleho Myeza <katleho.myeza@bbd.co.za>
2025-07-11 01:10:23 +0200

    fix

--- server/src/routes/simulation.routes.ts
+++ server/src/routes/simulation.routes.ts
@@ -5,29 +5,55 @@ import { createTransaction } from "../queries/transactions.queries";
 import { getAccountFromOrganizationUnit } from "../queries/auth.queries";
 import { logger } from "../utils/logger";
 import { getAllExistingAccounts, getLoanBalances } from "../queries/dashboard.queries";
+import { setLoanInterestRate } from "../queries/loans.queries";
 
 const router = Router();
 
 router.post("/start", async (req, res) => {
-    const { startingTime, startingBalance, fromAccountNumber } = snakeToCamelCaseMapper(req.body);
     try {
-        if (!startingTime || !startingBalance || !fromAccountNumber) {
+        const { startingTime } = snakeToCamelCaseMapper(req.body);
+
+        const balanceData = await getStartingBalance();
+
+        if (!startingTime || !balanceData) {
+          res.status(400).json({ error: "Bad Request: Missing required fields: starting_time, starting_balance, from_account_number" });
+          return;
+        }
+
+        const { initial_bank_balance, prime_rate } = balanceData;
+
+        if (!startingTime ) {
             res.status(400).json({ error: "Bad Request: Missing required fields: starting_time, starting_balance, from_account_number" });
             return;
         }
         initSimulation(startingTime + 10); // Offset by 10ms to account for minor network/request latency
+        setLoanInterestRate(Number(prime_rate))
+        const fromAccountNumber = await getAccountFromOrganizationUnit('thoh').then(account => account?.accountNumber);
         const toAccountNumber = await getAccountFromOrganizationUnit('commercial-bank').then(account => account?.accountNumber);
         if (!toAccountNumber) {
             res.status(404).json({ error: "Commercial bank account not found" });
             return;
         }
-        await createTransaction(fromAccountNumber, toAccountNumber, startingBalance, `Simulation start with balance ${startingBalance}`, 'thoh', 'commercial-bank')
+        await createTransaction(fromAccountNumber!, toAccountNumber, initial_bank_balance, `Simulation start with balance ${initial_bank_balance}`, 'thoh', 'commercial-bank')
         res.status(200).send(getDateTimeAsISOString());
     } catch (error) {
         logger.error("Error starting simulation:", error);
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
+
+const getStartingBalance = async (): Promise<{prime_rate:number,initial_bank_balance:number} | null> => {
+  try {
+    const response = await fetch('https://thoh-api.projects.bbdgrad.com/api/bank/initialization');
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+    const data = await response.json();
+    return (data) || null;
+  } catch (error) {
+    console.error('Error fetching starting balance:', error);
+    return null;
+  }
+};
 
 router.post("/end", async (req, res) => {
     try {
