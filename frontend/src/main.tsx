4989bd9164e03c519f8cc3dcf7e2e65ccb76718f
Thabang Modisaotsile <thabang.modisaotsile@standardbank.co.za>
2025-07-10 15:54:42 +0200

    removed statement

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-console.log('VITE_CLIENT_ID:', import.meta.env.VITE_CLIENT_ID);
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
