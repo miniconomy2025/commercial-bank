788ce172932509b52c3d07484c50810391fc1f4c
Thabang Modisaotsile <thabang.modisaotsile@standardbank.co.za>
2025-07-10 14:58:56 +0200

    update workflow

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log('VITE_CLIENT_ID:', import.meta.env.VITE_CLIENT_ID);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
