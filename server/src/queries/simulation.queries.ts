73708ccac64d1d714038ba38f919c37ef62dd804
Future-Phahlamohlaka <mamatankane.phahlamohlaka@bbd.co.za>
2025-07-12 20:39:25 +0200

    Add thohAccountNumber to appConfig and update simulation logic to use investment_value

--- server/src/queries/simulation.queries.ts
+++ server/src/queries/simulation.queries.ts
@@ -1,9 +1,12 @@
+import appConfig from "../config/app.config";
 import db from "../config/db.config";
 
 export const resetDB = async (time: number): Promise<void> => {
   await db.none(`
     DELETE FROM accounts;
     INSERT INTO accounts (account_number, team_id, notification_url, created_at)
-    VALUES (generate_unique_account_number(), 'commercial-bank', 'api/notify', 1234)
+    VALUES 
+      (generate_unique_account_number(), 'commercial-bank', 'api/notify', $1),
+      (${appConfig.thohAccountNumber}, 'thoh', 'api/notify', $1)
   `, [time]);
 };
